6a5adfc3a266c6d01ef768817cf3eefcae4f01c9
Agnogh <agnogh_orion@yahoo.com>
2026-01-13 23:56:10 +0100

    Add games list page and routing

--- games/views.py
+++ games/views.py
@@ -8,3 +8,8 @@ from .models import Game
 def game_detail(request, slug):
     game = get_object_or_404(Game, slug=slug)
     return render(request, "games/game_details.html", {"game": game})
+
+
+def game_list(request):
+    games = Game.objects.all()
+    return render(request, "games/game_list.html", {"games": games})
