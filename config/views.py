b089b3824bcdbfcd955c51cf1fd7bff0b7d40de1
Agnogh <agnogh_orion@yahoo.com>
2026-04-10 01:46:29 +0200

    Add signup form and signup view scaffolding

--- config/views.py
+++ config/views.py
@@ -1,9 +1,24 @@
 # helper module
 # render takes request + tempalte and combines. Then returns html in browser
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
 # defining function named 'home' with object 'request'
 
 
 def home(request):
     return render(request, "home.html")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = SignUpForm()
+
+    return render(request, "registration/signup.html", {"form": form})
