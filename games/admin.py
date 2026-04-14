a97d9ef0eeeb83eed3e89785bca8a7d4926f504e
Agnogh <agnogh_orion@yahoo.com>
2026-04-15 01:40:16 +0200

    Register Review model in Django admin

--- games/admin.py
+++ games/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 from .models import Game
 from .models import Category
+from .models import Review
 
 admin.site.register(Game)
 admin.site.register(Category)
+admin.site.register(Review)
